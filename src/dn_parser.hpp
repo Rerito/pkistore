10aed5f0735ed2b42aabfbda3122ecd5cec8c2e2
Seth Heeren <sgheeren@gmail.com>
2015-10-29 00:31:57 +0100

    Cleanup of grammar for legibility

--- src/dn_parser.hpp
+++ src/dn_parser.hpp
@@ -20,36 +20,23 @@ namespace pkistore {
         dn_grammar_common() : dn_grammar_common::base_type(start) {
             using namespace qi;
 
-            // Low level rules
-            // Key can only contain alphanumerical characters and dashes
+            // syntax as defined in rfc1779
             key          = raw[ alnum >> *(alnum | '-') ];
 
-            // Rule for a fully escaped string (used as Attribute Value) => "..."
             char_escape  = '\\' >> (hexchar | dn_reserved_chars);
             quote_string = '"' >> *(char_escape | (char_ - dn_reserved_chars)) >> '"' ;
 
-            // Value is either:
-            // - A regular string (that can contain escaped sequences)
-            // - A fully escaped string (that can also contain escaped sequences)
-            // - An hexadecimal string
-            value =  quote_string 
-                  | '#' >> *hexchar
-                  | *(char_escape | (char_ - dn_reserved_chars))
-                  ;
+            value        =  quote_string 
+                         | '#' >> *hexchar
+                         | *(char_escape | (char_ - dn_reserved_chars))
+                         ;
 
-            // Higher level rules
-            rdn_pair = key >> '=' >> value;
+            rdn_pair     = key >> '=' >> value;
 
-            // A relative distinguished name consists of a sequence of pairs (Attribute = AttributeValue)
-            // Separated with a +
-            rdn = rdn_pair % qi::char_("+");
+            rdn          = rdn_pair % qi::char_("+");
+            dn           = rdn % qi::char_(",;");
 
-            // The DN is a set of RDNs separated by either a "," or a ";".
-            // The two separators can coexist in a given DN, though it is not
-            // recommended practice.
-            dn = rdn % qi::char_(",;");
-
-            start = skip(qi::ascii::space) [ dn ];
+            start        = skip(qi::ascii::space) [ dn ];
 
             BOOST_SPIRIT_DEBUG_NODES((start)(dn)(rdn)(rdn_pair)(key)(value)(quote_string)(char_escape))
         }
