ecd2759e444de624e44aba16780a232a3b791ec5
Remi Marty <rerito2512@gmail.com>
2015-11-01 16:04:28 +0100

    Work on CMake files and Google Test integration

---
+++ test/test_dn_parsing.cc
@@ -0,0 +1,13 @@
+
+#include "gtest/gtest.h"
+#include "dn_parser.hpp"
+
+TEST(DnParserTest, BasicDn) {
+  using namespace pkistore::parsing;
+  std::string basic_dn("OU=Sales, CN=John Smith, O=Dummy Inc., C=US");
+  ast::dn expected_dn = {{"OU", "Sales"}, {"CN", "John Smith"},
+                        {"O", "Dummy Inc."}, {"C", "US"}};
+  ast::dn parsed_dn = parse(basic_dn);
+  ASSERT_FALSE(parsed_dn.empty()) << "Parsing is not empty";
+  ASSERT_EQ(parsed_dn, expected_dn);
+}
